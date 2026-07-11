fb8cf3f0aab07e4057e33328f946f1a26992b943
rachit daksh <rachitdaksh@rachits-MacBook-Air.local>
2026-07-11 11:22:24 +0530

    added basic xcode project configuration and assets

---
+++ erpTests/erpTests.swift
@@ -0,0 +1,17 @@
+//
+//  erpTests.swift
+//  erpTests
+//
+//  Created by rachit daksh on 11/07/26.
+//
+
+import Testing
+@testable import erp
+
+struct erpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
